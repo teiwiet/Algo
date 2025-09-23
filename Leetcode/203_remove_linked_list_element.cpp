65744cfddb2c45546090f1b1b054d680a36c97a7
teiwiet <teiwiet@teiwiet.teiwiet>
2025-09-23 00:57:32 -0400

    update

--- Leetcode/203_remove_linked_list_element.cpp
+++ Leetcode/203_remove_linked_list_element.cpp
@@ -9,11 +9,6 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-ListNode* removeElement(ListNode* &head,int val){
-	ListNode* temp = head;
-	
-}
-
 int main(){
 	return 0;
 }
