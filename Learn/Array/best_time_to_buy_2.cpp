ba062542fa03941c183913e1669991cd0dd9fcaf
teiwiet <teiwiet33@gmail.com>
2025-11-23 02:36:58 -0500

    update

--- Learn/Array/best_time_to_buy_2.cpp
+++ Learn/Array/best_time_to_buy_2.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 class Solution{
 public:
-    int maxProfit(vector<int> prices){
+    int maxProfit(vector<int>& prices){
 		int max_profit = 0;
 		for(int i = 0;i<prices.size();i++){
 			if(prices[i]<prices[i+1]){
-				max_profit += prices[i+1] - prices[i];
+				max_profit += prices[i+1]-prices[i];
 			}
 		}
 		return max_profit;
-    }
+  }
 };
 
 int main(){
-    Solution s;
-    vector<int> prices {7,1,5,3,6,4};
-    cout << s.maxProfit(prices);
+	Solution *s = new Solution();
+	vector<int> prices {7,1,5,3,6,4};
+	cout << s->maxProfit(prices);
 }
