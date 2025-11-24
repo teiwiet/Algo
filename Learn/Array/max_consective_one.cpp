912f090f0c5afe16b4d73720adb2fcf92859400d
teiwiet <teiwiet33@gmail.com>
2025-11-24 10:03:58 -0500

    update

--- Learn/Array/max_consective_one.cpp
+++ Learn/Array/max_consective_one.cpp
@@ -5,18 +5,18 @@ using namespace std;
 class Solution {
 public:
     int findMaxConsectiveOnes(vector<int> nums){
-        int count = 0;
-        int res = 0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                count = 0;
-            }
-            else{
-                count++;
-                res = max(count,res);
-            }
-        }
-        return res;
+		int res = 0;
+		int count = 0;
+		for(int i = 0;i<=nums.size()-1;i++){
+			if(nums[i]==0){
+				count = 0;
+			}
+			else{
+				count++;
+				res = max(res,count);
+			}
+		}
+		return res;
     }
 };
 
---
+++ Learn/Recursion/generate_subsets.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void subset(string s="abc",string curr="",int i = 0){
+	if(i==s.length()){
+		cout << curr << " ";
+		return;
+	}
+	subset(s,curr,i+1);
+	subset(s,curr+s[i],i+1);
+}
+
+
+int main(){
+	string s;
+	s = "abc";
+	subset(s);
+}
