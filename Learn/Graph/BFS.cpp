132103a6456999aeee686ce4faa73e34a8458de7
teiwiet <teiwiet33@gmail.com>
2025-12-02 16:17:34 -0500

    update

--- Learn/Graph/BFS.cpp
+++ Learn/Graph/BFS.cpp
@@ -1,37 +1,31 @@
-#include <bits/stdc++.h>
+#include<iostream>
+
 using namespace std;
-void BFS(const vector<vector<int>>& adj, int start){
-    int V = adj.size();
-    vector<bool> visited(V, false);
-    queue<int> q;
-    q.push(start);
-    visited[start] = true;
-    while(!q.empty()){
-        int u = q.front();
-        q.pop();
-        cout << u << " ";
-        for(int v : adj[u]){
-            if(!visited[v]){
-                visited[v] = true;
-                q.push(v);
-            }
-        }
-    }
-    cout << "\n";
+
+void BFS(vector<int> adj[],int V,int s){
+	bool visited[V+1];
+	for(int i = 0;i<V;i++){
+		visited[i] = false;
+	}
+	queue<int> q;
+	visited[s] = true;
+	q.push(s);
+
+	while(!q.empty()){
+		int u = q.front();
+		q.pop();
+
+		cout << u << " ";
+		for(int v: adj[u]){
+			if(visited[v]==false){
+				visited[v] = true;
+				q.push(v);
+			}
+		}
+	}
 }
 
+
 int main(){
-    int V, E, start;
-    cin >> V >> E >> start;
-    vector<vector<int>> adj(V);
-    for(int i = 0; i < E; i++){
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    for(auto &list : adj)
-        sort(list.begin(), list.end());
-    BFS(adj, start);
-    return 0;
+
 }
---
+++ Learn/Graph/adjacency_list.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void addEdge(vector<int> adj[],int u,int v){
+	adj[u].push_back[v];
+	adj[v].push_back[u];
+}
+
+int main(){
+	int v = 4;
+	vector<int> adj[v];
+}
